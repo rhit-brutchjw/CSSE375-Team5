a2e51582db1278c6ef5d9fff36493a7680dc753a
rameydj <rameydj@rose-hulman.edu>
2023-04-20 19:53:31 -0400

    Refactored alternatingFitnessCalc into the strategy pattern created earlier.

---
+++ src/main/java/FitnessAlternating.java
@@ -0,0 +1,30 @@
+public class FitnessAlternating implements FitnessStrategy{
+    @Override
+    public void doFitnessCalculation(Chromosome target, Chromosome individual) {
+        int bestScore = 0;
+        int curScore = 0;
+        int[][] indGenes = individual.getGenes();
+        int previous;
+        if(indGenes[0][0] == 0) {
+            previous = 1;
+        } else {
+            previous = 0;
+        }
+        for (int i = 0; i < indGenes.length; i++) {
+            for (int j = 0; j < indGenes[i].length; j++) {
+                if(curScore == 0 && indGenes[i][j] != previous) {
+                    curScore++;
+                } else if(curScore > 0 && indGenes[i][j] != previous) {
+                    curScore++;
+                } else if(curScore > 0 && indGenes[i][j] == previous) {
+                    if (curScore > bestScore) {
+                        bestScore = curScore;
+                    }
+                    curScore = 0;
+                }
+                previous = indGenes[i][j];
+            }
+        }
+    }
+
+}
